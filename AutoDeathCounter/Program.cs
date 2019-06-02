07621252b3e9208b93bf19a1dcea7d1ea12d1d5c
Fandus Atipov <aisatan.ne@live.com>
2019-06-02 14:02:30 +0300

    Add project files.

---
+++ AutoDeathCounter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoDeathCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
